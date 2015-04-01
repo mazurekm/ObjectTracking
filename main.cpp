b3c99567f172d172efc555a755502609c701bdf8
Michał <mazur@myown.test>
2015-04-01 22:29:13 +0200

    cmake changes, c++11 support

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
+#include <memory>
 
 int main()
 {
-	cv::VideoCapture cap(0);
-	if(false == cap.isOpened())
+	std::shared_ptr<cv::VideoCapture> cap(new cv::VideoCapture(0));
+	if(false == cap->isOpened())
 	{
 		return -1;
 	}
 
 
-	double fps = cap.get(CV_CAP_PROP_FPS);
 	cv::namedWindow("Video", CV_WINDOW_AUTOSIZE);
 
 	while(true)
 	{
 		cv::Mat frame;
-		cap.read(frame);
+		cap->read(frame);
 		cv::imshow("Video", frame);
 
 		if(cv::waitKey(30) == 27)
