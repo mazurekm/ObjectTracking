2ca052feb1d42d32e20a302704c650fe5000167e
Michał <mazur@myown.test>
2015-05-14 00:56:08 +0200

    merge

--- Algorithms/Tld/TldAlgorithm.cpp
+++ Algorithms/Tld/TldAlgorithm.cpp
@@ -1,26 +1,45 @@
 #include "TldAlgorithm.h"
+#include <FrameProcessing/ImageMarker.h>
 
 CTldAlgorithm::CTldAlgorithm(const CTransformContainer &container, const std::string &winName) : CAbstractAlgorithm(container, winName) 
 {
 	
 }
 
-
 void CTldAlgorithm::perform(CVideoLoader &loader)
 {
-	cv::namedWindow(m_winName, CV_WINDOW_AUTOSIZE);
-	
+	/*cv::namedWindow(m_winName, CV_WINDOW_AUTOSIZE);
+	CImageMarker::getInstance()->setWinName(m_winName);	
+
+	cv::Mat frame;
 	while(true)
 	{
-		cv::Mat frame = loader.getNextFrame();
-		m_container.perform(frame);
-		cv::imshow(m_winName, frame);
+		if(false == CImageMarker::getInstance()->isMarkerActive())
+		{	
+			frame = loader.getNextFrame();
+			//m_container.perform(frame);
+			CImageMarker::getInstance()->setFrame( frame );
+			cv::imshow(m_winName, frame);
+		}
+		else
+		{
+			cv::imshow(m_winName, CImageMarker::getInstance()->getFrame() );
+		}
 
-		if(cv::waitKey(30) == 27)
+		std::vector<cv::Mat> imgVec = CImageMarker::getInstance()->getImgVec();
+		
+
+		if(imgVec.size() > 0)
+		{
+			cv::namedWindow("aaa", CV_WINDOW_AUTOSIZE);
+			cv::imshow("aaa",*imgVec.begin());		
+		}
+
+		if(cv::waitKey(20) == 27)
 		{
 			break;
 		}	
 	}
-		
+	*/	
 }
 
