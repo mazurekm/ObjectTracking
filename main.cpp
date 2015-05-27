408153168b132e8d787dbf79b0d176d72cbac862
Michał <mazur@myown.test>
2015-05-27 18:20:00 +0200

    linking problems solved

--- main.cpp
+++ main.cpp
@@ -3,27 +3,14 @@
 #include <FrameProcessing/PatternController.h>
 #include <FrameProcessing/State.h>
 #include <Algorithms/Tld/TldAlgorithm.h>
+#include <Algorithms/OptFlow/OpticalFlow.h>
 #include <cassert>
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-	if(1 >= argc)
-	{
-		assert(0&&"Wrong input parameter");
-	}
-
-	CVideoLoader loader(argv[1]);
-
-	CTransformContainer container;
-	container.addTransform( std::shared_ptr<CImageTransform>(new MedianBlur () ) );
-
-	cv::namedWindow("bbb", CV_WINDOW_AUTOSIZE);
-	CPatternController::getInstance().setWinName("bbb");	
-	CTldAlgorithm tldAlgorithm(container, "tld");
-
-	std::shared_ptr<State> state( new MarkState() );
-	while (nullptr != state) {
-		state = state->handle(loader, tldAlgorithm);
-	}
+	CVideoLoader loader;
+	COpticalFlow flow("Flow");
+	flow.perform(loader);
+	return 0;
 }
